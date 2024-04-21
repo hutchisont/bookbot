c0e1ede5246b240f7e87bb75b0542c8d6fc87ec2
hutchisont <24536068+hutchisont@users.noreply.github.com>
2024-04-21 02:20:40 -0500

    feat: get per character count of input

--- main.py
+++ main.py
@@ -3,11 +3,25 @@ def count_words_in_string(input: str) -> int:
     return count
 
 
+def per_character_count(input: str) -> dict[str, int]:
+    input = input.lower()
+    tracker: dict[str, int] = {}
+
+    for char in input:
+        if char in tracker:
+            tracker[char] += 1
+        else:
+            tracker[char] = 1
+
+    return tracker
+
+
 def main():
     with open("books/frankenstein.txt") as f:
         data = f.read()
         count = count_words_in_string(data)
         print(count)
+        print(per_character_count(data))
 
 
 if __name__ == "__main__":
