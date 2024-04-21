c8169d2c88eb444b1b1e462ebae8a81ef91f02b8
hutchisont <24536068+hutchisont@users.noreply.github.com>
2024-04-21 16:59:11 -0500

    feat: print report on the book

--- main.py
+++ main.py
@@ -16,12 +16,32 @@ def per_character_count(input: str) -> dict[str, int]:
     return tracker
 
 
+def sort_fn(dictionary: dict[str, int]) -> int:
+    for value in dictionary.values():
+        return value
+
+
 def main():
-    with open("books/frankenstein.txt") as f:
+    book = "books/frankenstein.txt"
+    with open(book) as f:
         data = f.read()
-        count = count_words_in_string(data)
-        print(count)
-        print(per_character_count(data))
+        words_count = count_words_in_string(data)
+        chars_count = per_character_count(data)
+
+        print(f"*** Begin report for {book} ***")
+        print(f"{words_count} words found in the document")
+
+        sorter = []
+
+        for entry, count in chars_count.items():
+            sorter.append({entry: count})
+
+        sorter.sort(reverse=True, key=sort_fn)
+        for item in sorter:
+            for key, val in item.items():
+                print(f"'{key}' character was found {val} times")
+
+        print(f"*** End report for {book} ***")
 
 
 if __name__ == "__main__":
