6e342c6aeb13d4c46d613351f407f9015e2ae390
hutchisont <24536068+hutchisont@users.noreply.github.com>
2024-04-21 02:02:44 -0500

    feat: count words in the file

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello world")
+
+def count_words_in_string(input):
+    count = len(input.split())
+    return count
+
+
+def main():
+    with open("books/frankenstein.txt") as f:
+        data = f.read()
+        count = count_words_in_string(data)
+        print(count)
+
+
+if __name__ == "__main__":
+    main()
